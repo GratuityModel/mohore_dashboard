d0f5ec28517c215c4ab594d24e1d10de75d295ba
GratuityModel <gratuitymodel@gmail.com>
2026-02-18 22:00:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from pipeline2 import *
+from pipeline import *
 from PIL import Image
 
 # ==========================================================
@@ -1604,3 +1604,4 @@ with main_col:
             """
         )
     
+
